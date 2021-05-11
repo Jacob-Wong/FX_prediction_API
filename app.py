2e6d5f29f524e89ac9acbbb8b9c1970bd3db931c
Jacob-Wong <jacobwfk@gmail.com>
2021-05-12 02:41:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import numpy as np
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import tensorflow as tf
 import datetime
 from sklearn.preprocessing import MinMaxScaler
@@ -57,6 +58,23 @@ def DefineInterval(interval):
 
 
 app = FastAPI()
+
+# cors
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# cors
 
 
 @app.get('/')  # basic get view
