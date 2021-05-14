58e66396a0438870273f01cbe7a8587252d6684d
Jacob-Wong <jacobwfk@gmail.com>
2021-05-14 17:50:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -145,5 +145,6 @@ async def read_user_item(fx_pair: str, interval: str):
         "prediction": predictions,
         'trend': trend,
         'action': action,
-        "color": color
+        "color": color,
+        "predicting_date": predicting_date,
     }
