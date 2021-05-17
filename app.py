a5ba39c9927e088390b549fdfcb395b0b4d069da
Jacob-Wong <jacobwfk@gmail.com>
2021-05-17 13:14:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def DataPreprcess(fx_pair, interval):
     currency_data = currency_data[-31:]
     currency_data = currency_data[:30]
     date_saver = date_saver[-31:]
-    date_saver = date_saver[:30]
+    date_saver, predicting_date = date_saver[:30], data_saver[-1]
 
     last_price = currency_data.iloc[-1]
     last_price = last_price.values[0]
@@ -38,13 +38,14 @@ def DataPreprcess(fx_pair, interval):
     date_saver.values.tolist()
     from_date, to_date = date_saver.iloc[0].name.strftime('%Y-%m-%d'), date_saver.iloc[-1].name.strftime('%Y-%m-%d')
 
-    predicting_date = datetime.datetime.strptime(to_date, '%Y-%m-%d')
-    if (interval == 'd'):
-        x = datetime.timedelta(days=1)
-    elif (interval == 'wk'):
-        x = datetime.timedelta(weeks=1)
-    predicting_date = predicting_date + x
-    predicting_date = predicting_date.strftime('%Y-%m-%d')
+    # predicting_date = datetime.datetime.strptime(to_date, '%Y-%m-%d')
+    # if (interval == 'd'):
+    #     x = datetime.timedelta(days=1)
+    # elif (interval == 'wk'):
+    #     x = datetime.timedelta(weeks=1)
+    # predicting_date = predicting_date + x
+    # predicting_date = predicting_date.strftime('%Y-%m-%d')
+    predicting_date = predicting_date.iloc[0].name.strftime('%Y-%m-%d')
 
     # get the last 30 day price values and convert the dataframe to an array
     last_30 = currency_data.values.tolist()
